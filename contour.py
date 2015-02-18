721a8e46207f6721c09c59e7eaa137fd477f94dd
eaglespy21 <saurabhhindlekar@hotmail.com>
2015-02-18 12:11:14 +0530

    modules

--- contour.py
+++ contour.py
@@ -1,12 +1,13 @@
 import numpy as np 
 import cv2 
-im = cv2.imread('crop0.jpg')
+im = cv2.imread('worko.jpg')
 imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 ret,thresh = cv2.threshold(imgray, 127, 255, 0)
 contours, hierarchy =cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-cnt = contours[1]
+cnt = contours[0]
 cv2.drawContours(im, [cnt], 0, (0,255,0), 3)
 cv2.imshow("hello", im)
 #cv2.imshow("contours",contours)
 cv2.imshow("cnt[0]",cnt[1])
+cv2.imwrite('workoc.jpg', im)
 cv2.waitKey(0)
